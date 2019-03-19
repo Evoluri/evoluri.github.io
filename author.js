cb037a5e9f66daaa5e0f899eb9ff40d9aedf716f
Evoluri <47928234+Evoluri@users.noreply.github.com>
2019-03-19 15:23:22 -0300

    Delete author.js

--- author.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var evo_xName = 'Coign';
-	var evo_xLink = 'https://google.com';
-	var evo_xQuery = '?ref=';
-	var evo_xSy = 'Desenvolvido por:';
-	var evo_PageUrl = document.URL;
-	document.getElementById("evo--author").innerHTML = '<small>'+evo_xSy+' <a href="'+evo_xLink+evo_xQuery+evo_PageUrl+'" target="_blank">'+evo_xName+'</a></small>';
-});
