5f2044d2c9b157bdc9847c63a9e10d1c360ab636
Evoluri <47928234+Evoluri@users.noreply.github.com>
2019-03-19 15:34:45 -0300

    Add files via upload

--- api/author.js
+++ api/author.js
@@ -1,7 +1,6 @@
-
-    var evo_xName = 'Coign';
-	var evo_xLink = 'https://google.com';
-	var evo_xQuery = '?ref=';
-	var evo_xSy = 'Desenvolvido por:';
-	var evo_PageUrl = document.URL;
-	document.getElementById("evo--author").innerHTML = '<small>'+evo_xSy+' <a href="'+evo_xLink+evo_xQuery+evo_PageUrl+'" target="_blank">'+evo_xName+'</a></small>';
+var evo_xName = 'Coign';
+var evo_xLink = 'https://google.com';
+var evo_xQuery = '?ref=';
+var evo_xSy = 'Desenvolvido por:';
+var evo_PageUrl = document.URL;
+document.getElementById("evo--author").innerHTML = '<small>'+evo_xSy+' <a href="'+evo_xLink+evo_xQuery+evo_PageUrl+'" target="_blank" style="color:rgba(255,255,255,0.6);">'+evo_xName+'</a></small>';
